43eda9fa70369cff2efa53bd695e0963315edf8e
Marcos Pinheiro <marcos.pinheiro@outlook.com>
2016-12-14 23:47:25 -0200

    Atualizacao gitignore

---
+++ src/main/java/br/com/fiap/cloud/dogcare/controller/dto/VaccineDTO.java
@@ -0,0 +1,17 @@
+package br.com.fiap.cloud.dogcare.controller.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data @AllArgsConstructor
+public class VaccineDTO {
+
+	private String name;
+	
+	//private LocalDate date;
+	
+	public VaccineDTO() {
+		
+	}
+
+}
